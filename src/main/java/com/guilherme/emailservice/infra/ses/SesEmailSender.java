9f4ecbb2fad65d7629bb1cf0b312934e3192b966
Guilherme Santana <guiguilsantana@gmail.com>
2024-03-10 00:13:14 -0300

    feat: Create project EmailService

---
+++ src/main/java/com/guilherme/emailservice/infra/ses/SesEmailSender.java
@@ -0,0 +1,36 @@
+package com.guilherme.emailservice.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.guilherme.emailservice.adapters.EmailSenderGateway;
+import com.guilherme.emailservice.core.excpetions.EmailServiceExcpetion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void senEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("guiguilsantana@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body))));
+
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        }  catch (AmazonServiceException exception){
+            throw  new EmailServiceExcpetion("Falha ao enviar o e-mail", exception);
+        }
+    }
+}
